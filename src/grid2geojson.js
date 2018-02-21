f819ace225ee25101f36101986d49537b436c6eb
Aruk Pupatee <aruk.pupatee@gmail.com>
2018-02-21 20:01:45 +0700

    Insert function toGeoJSON()

--- src/grid2geojson.js
+++ src/grid2geojson.js
@@ -1,4 +1,56 @@
-exports.toGeoJSON = (lats, lons, data=null, padding=true) => {
+exports.toGeoJSON = (lats, lons, data, padding=true) => {
     var arr = [];
 
+    if(padding){
+        for(let latIdx = 0; latIdx < lats.length; latIdx++){
+            for(let lonIdx = 0; lonIdx < lons.length; lonIdx++){
+                let lat1 = lats[latIdx]
+                let lon1 = lons[lonIdx];
+                let lat2, lon2;
+                if(latIdx != lats.length-1){
+                    lat2 = lats[latIdx+1];
+                } else {
+                    lat2 = lats[latIdx] + (lats[latIdx] - lats[latIdx-1]); 
+                }
+                if(lonIdx != lons.length-1){
+                    lon2 = lons[latIdx+1];
+                } else {
+                    lon2 = lons[latIdx] + (lons[latIdx] - lons[latIdx-1]); 
+                }
+                let feature = { type: "Feature" };
+                feature.properties = { value: data[latIdx][lonIdx] };
+                feature.geometry = {
+                    "type": "Polygon",
+                    "coordinates": [[
+                        [lat1, lon1],
+                        [lat1, lon2],
+                        [lat2, lon1],
+                        [lat2, lon2]
+                    ]]
+                };
+                arr.append(feature);
+            }
+        }
+    } else {
+        for(let latIdx = 0; latIdx < lats.length-1; latIdx++){
+            for(let lonIdx = 0; lonIdx < lons.length-1; lonIdx++){
+                let lat1 = lats[latIdx]
+                let lon1 = lons[lonIdx];
+                let lat2 = lats[latIdx+1];
+                let lon2 = lons[latIdx+1];
+                let feature = { type: "Feature" };
+                feature.properties = { value: data[latIdx][lonIdx] };
+                feature.geometry = {
+                    "type": "Polygon",
+                    "coordinates": [[
+                        [lat1, lon1],
+                        [lat1, lon2],
+                        [lat2, lon1],
+                        [lat2, lon2]
+                    ]]
+                };
+                arr.append(feature);
+            }
+        }
+    }
 };
